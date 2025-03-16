38678e49a18c97aeb406cfe96a4981b475891fbd
Zain-Abbas <zain786@outlook.com>
2025-03-15 17:59:24 -0700

    (compression works fine, need to radically change rabin-karp algorithm to the summatation model in the pdf.)

--- src/FindAllMatches.java
+++ src/FindAllMatches.java
@@ -6,109 +6,63 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FindAllMatches {
-    static class S {
-        final int N, M, hash;
-        final byte[] needle, haystack;
-        final static int MOD = 1000000007;
-        final static int RAD = 101;
+    // internal method only used in 'find'.
+    // requires fr and rank to be zeroed.
+    private static void compress(byte[] xs, int[] fr, byte[] rank) {
+        byte x = 0;
+        for (var n : xs) fr[n]++;
+        for (int i = 1; i < 101; i++) rank[i] = fr[i] > 0 ? x++ : x;
+        for (int i = 0; i < xs.length; i++) xs[i] = rank[xs[i]];
+    }
 
-        public S(int N, int M, byte[] needle, byte[] haystack) {
-            this.N = N;
-            this.M = M;
-            this.needle = needle;
-            this.haystack = haystack;
-            // create array to store frequencies of values
-            // 101 -> maximum value is 100
-            var fr = new int[101];
-            // create array to store
-            var rank = new byte[101];
-            // method to y-compress
-            System.out.println("compressed needle");
-            compress(needle, fr, rank);
-            Arrays.fill(fr, 0);
+    // Return a list of all *potential* matches (need to be checked).
+    static ArrayList<Integer> find(byte[] n, byte[] h0) {
+        final int MOD = 101;
+        final int RAD = 10193; // least prime greater than 101^2
+        long hash = 0, bigrad = 1, temp = RAD % MOD;
+        byte[] h = new byte[n.length];
+        var result = new ArrayList<Integer>();
+        // initialize
+        //  hash - hash of the needle (n)
+        //  bigrad - RAD ^ (M - 1), where M is |n|
+        var fr = new int[101];
+        var rank = new byte[101];
+        compress(n, fr, rank);
+        for (var b : n) hash = (((hash * (RAD % MOD)) % MOD) + b) % MOD;
+        for (int p = n.length - 1; p > 0; p >>= 1) {
+            if ((p & 1) == 1) bigrad = (bigrad * temp) % MOD;
+            temp = (temp * temp) % MOD;
+        }
+        // Rabin-Karp
+        //  k - hash of a part of haystack
+        //      to be compared with 'hash' (for the needle (n))
+        for (int i = 0; i < h0.length - n.length; i++) {
             Arrays.fill(rank, (byte) 0);
-            System.out.println("compressed haystack");
-            compress(haystack, fr, rank);
-            int h = 0;
-            for (var b : needle) h = (h * RAD + b) % MOD;
-            hash = h;
+            Arrays.fill(fr, (byte) 0);
+            System.arraycopy(h0, i, h, 0, n.length);
+            compress(h, fr, rank);
+            long k = 0;
+            for (var b : h) k = (k * (RAD % MOD) + b) % MOD;
+            if (k == hash) result.add(i);
+            // x_(i+1) = (x_i - t_i * R^(M-1)) * R + t_(i+M)
+            // k = ((k + MOD - h[i] * ((RAD % MOD) * RAD) % MOD) * RAD + h[i + n.length]) % MOD;
         }
-
-        private void compress(byte[] xs, int[] fr, byte[] rank) {
-            byte x = 0;
-            // count the frequency of each value
-
-            // needle: [5,6,5,13,13,9]
-            // fr[5] = 2
-            // fr[6] = 1
-            //
-
-            // will count frequency of values in the array
-            // fr[0] = 2, fr[1] = 3
-            // fr[0]++ ->
-            for (var n : xs) {
-                fr[n]++;
-                //System.out.println("fr[n]: " + fr[n]);
-            }
-            for (var n : xs) {
-                System.out.println("fr["+ n +"] = " + fr[n]);
-            }
-
-            for (int i = 1; i < 101; i++) {
-                // rank[1] = 0
-                rank[i] = x;
-                System.out.println("rank["+ i +"] = " + rank[i]);
-                // increment x if current rank (x) less than frequency of i in fr array
-                if (x < fr[i]) x++;
-            }
-            for (int i = 0; i < xs.length; i++) xs[i] = rank[xs[i]];
-        }
-
-        public ArrayList<Integer> solve() {
-            // use Rabin-Karp to enumerate all matches
-            var ms = new ArrayList<Integer>();
-            throw new RuntimeException("need to solve");
-        }
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
-        /*
-        if (args.length != 1) {
-            System.out.println("Usage: java FindAllMatches.java <test_file_name>");
-            return;
-        }
-
-         */
-
-        BufferedReader in = new BufferedReader(new FileReader(args[0]));
-        String[] head = in.readLine().split(" ");
-        int N = Integer.parseInt(head[0]);
-        int M = Integer.parseInt(head[1]);
-        System.out.println(N + " " + M);
-        // needle -> pattern
-        byte[] needle = new byte[M];
-        byte[] haystack = new byte[N];
-        for (int i = 0; i < N; i++) {
-            int n = Integer.parseInt(in.readLine());
-            haystack[i] = (byte) n;
-           // System.out.println("given pattern: " + needle[i]);
-        }
-        for (int i = 0; i < M; i++) {
-            int m = Integer.parseInt(in.readLine());
-            needle[i] = (byte) m;
-          //  System.out.println("given string: " + haystack[i]);
-
-        }
+        var in = new BufferedReader(new FileReader(args[0]));
+        var head = in.readLine().split(" ");
+        int N = Integer.parseInt(head[0]), M = Integer.parseInt(head[1]);
+        byte[] haystack = new byte[N], needle = new byte[M];
+        for (int i = 0; i < N; i++) haystack[i] = (byte) Integer.parseInt(in.readLine());
+        for (int i = 0; i < M; i++) needle[i] = (byte) Integer.parseInt(in.readLine());
         in.close();
-
-        // YOUR CODE
-        S s = new S(N, M, needle, haystack);
-        /*
-        var matches = s.solve();
-        for (var match : matches) System.out.printf("%d ", match);
-         */
+        var matches = find(needle, haystack);
+        for (var match : matches)
+            // inaccuracy
+            System.out.printf("%d ", match);
+        System.out.println();
     }
-
-
 }
 
